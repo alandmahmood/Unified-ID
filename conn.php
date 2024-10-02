496e6abaa25d04927aea9ca0763d0e0646d02161
alandmahmood <aland.aramf21@komar.edu.iq>
2024-10-02 19:20:36 +0300

    server cred update

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","admin","RootPass","universal_id");
+$conn = mysqli_connect("localhost","UnifiedID","UnifiedID123pass","universal_id");
 
 if (!$conn){
     die("connection failed".mysqli_connect_error());
