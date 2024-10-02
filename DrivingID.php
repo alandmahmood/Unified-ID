7323028ace776cd48cfb3b0b767560604bcaa585
alandmahmood <aland.aramf21@komar.edu.iq>
2024-10-02 21:51:21 +0300

    done plz done

--- DrivingID.php
+++ DrivingID.php
@@ -45,76 +45,76 @@ if (isset($_GET["did"])) {
         }
     </style>
 </head>
-    <!-- Navbar -->
-    <?php include 'nav.php'; ?>
 
-
-<body class="flex flex-col min-h-screen">
-    <!-- Main Content -->
-    <div class="flex-grow w-full px-4 md:px-36">
-        <div class="w-full flex flex-row items-center text-3xl py-4 px-4 font-serif">
-            <div class="w-auto place-items-start text-gray-900">
-                Republic of Iraq
+<body>
+    <div class=" h-full w-full min-h-screen bg-auto">
+        <div class="min-h-full w-full px-36 h-screen">
+            <div class="w-full flex flex-row items-center  text-3xl py-4 px-4 font-serif ">
+                <div class="w-auto place-items-start" style="color:white;">
+                    Republic of Iraq
+                </div>
+                <div class="w-auto py-4 px-4 place-items-start">
+                    <img src="Coat_of_arms_of_Iraq.svg" alt="Logo" class="h-16 w-16 mr-2">
+                </div>
             </div>
-            <div class="w-auto py-4 px-4 place-items-start">
-                <img src="Coat_of_arms_of_Iraq.svg" alt="Logo" class="h-16 w-16 mr-2">
-            </div>
-        </div>
-        <div class="container blurred">
-            <div class="bg-gray-200 shadow-lg rounded-3xl w-full" style="padding: 20px;">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-md-4 d-flex justify-content-center align-items-center rounded-3xl" 
-                             style="background-color:#113f59;">
-                            <div class="text-center">
-                                <img class="w-40 h-40 text-gray-200" src="images/<?php echo $row["image_name"]; ?>" 
-                                     alt="Person Photo">
-                                <p class="text" style="color:white;"><?php echo $row["nid"]; ?></p>
+            <div class="container blurred">
+                <!-- id part -->
+                <div class="bg-gray-200 shadow-lg rounded-3xl w-12/12 h-full " style="padding: 20px;">
+                    <div class="py-6 ">
+        
+                        <div class="w-full flex flex-row justify-between text-2xl py-4 px-6 rounded-3xl shadow-lg" style="background-color: #113f59;">
+                            <div class="text-2xl"><a href='index.php' class=" text-white">Universal ID</a></div>
+                            <div class="flex flex-row space-x-4">
+                                <a href='nationalID.php?nid=<?php echo $row["nid"]; ?>' class=" text-white">National ID</a>
+                                <a href='HealthID.php?hid=<?php echo $row["hid"]; ?>' class=" text-white">Health ID</a>
+                                <a href='DrivingID.php?did=<?php echo $row["did"]; ?>' class=" text-white">Driving License ID</a>
                             </div>
                         </div>
-                        <div class="col-12 col-md-5">
-                            <div class="bg-gray-200 p-4 mx-4 my-4 rounded-md flex text-black">
-                                <div class="flex-1">
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Full Name: <?php echo $row["full_name"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Address: <?php echo $row["address"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">DOB: <?php echo $row["dob"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Age: <?php echo $row["age"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Type: <?php echo $row["li_type"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Phone number: <?php echo $row["phone"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Issue Date: <?php echo $row["driving_iss"]; ?></p>
-                                            <p class="text-xl mb-2 md:text-lg" style="font-size: 1rem;">Expire Date: <?php echo $row["driving_exp"]; ?></p>
-
-
-
+                    </div>
+                    <div class="container">
+                        <div class="row ">
+                            <div class="col-2 col-md-2 d-flex justify-content-center align-items-center rounded-3xl " style="background-color:#113f59;">
+                                <div class="text-center">
+                                    <img class="w-40 h-40 " src="images/<?php echo $row["image_name"]; ?>" alt="Person Photo">
+                                    <!-- Card ID paragraph -->
+                                    <p class="text" style="color:white;"><?php echo $row["nid"]; ?></p>
                                 </div>
-
                             </div>
-                        </div>
-                        <div class="col-12 col-md-3 d-flex justify-content-center align-items-center rounded-3xl" 
-                             style="background-color:#1f72a2;">
-                            <img src="https://api.qrserver.com/v1/create-qr-code/?data=<?php echo "http://localhost/advanced/advanced/advanced/DrivingID.php?nid=" . $row['nid'] ?>&amp;size=300x300" 
-                                 class="qr-code img-thumbnail img-responsive" />
+                            <div class="col-7 cold-md-7">
+                                <div class="bg-gray-200 p-4 mx-4 my-8 rounded-md flex">
+                                    <!-- Content on the left -->
+                                    <div class="flex-1">
+                                        <p class="text-xl mb-2">Full Name: <?php echo $row["full_name"]; ?></p>
+                                        <p class="text-xl mb-2">Address: <?php echo $row["address"]; ?></p>
+                                        <p class="text-xl mb-2">DOB: <?php echo $row["dob"]; ?></p>
+                                        <p class="text-xl mb-2">Age: <?php echo $row["age"]; ?></p>
+                                        <p class="text-xl mb-2">Type: <?php echo $row["li_type"]; ?></p>
+                                        <p class="text-xl mb-2">Phone number: <?php echo $row["phone"]; ?></p>
+                                        <p class="text-xl mb-2">Issue Date: <?php echo $row["driving_iss"]; ?></p>
+                                        <p class="text-xl mb-2">Expire Date: <?php echo $row["driving_exp"]; ?></p>
+
+
+
+                                    </div>
+
+                                    <!-- Photo on the right top corner -->
+                                </div>
+                            </div>
+                            <div class="col-3 cold-md-3 d-flex justify-content-center align-items-center rounded-3xl" style="background-color:#1f72a2;">
+                                <!-- Information on the right -->
+
+                                <img src="https://api.qrserver.com/v1/create-qr-code/?data=<?php echo "http://u-id.online/NationalID.php?nid=" . $row['nid'] ?>&amp;size=300x300" class="qr-code img-thumbnail img-responsive" />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <br>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-    <!-- Footer Inclusion -->
-    <?php include 'footer.php'; ?>
-
-    <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" 
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" 
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" 
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
-            crossorigin="anonymous"></script>
 </body>
 
 </html>
